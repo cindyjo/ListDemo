71b73ec1ea3777c36a88c6214542260662d73c50
Cindy Jo <sungaej@gmail.com>
2015-07-07 11:45:19 -0700

    Added Binary_tree Class and a function add

--- binary_tree.js
+++ binary_tree.js
@@ -3,3 +3,37 @@ var BSTNode = function(val){
   var right = null;
   var left = null;
 }
+
+var Tree = function(){
+	var root = null;
+	var add = function(val) {
+		var newNode = new Node(val);
+		var current = root;
+		
+		if(root == null) {
+			root = newNode;
+			return true;
+		}
+		
+		while(current){
+			if(newNode.val < current.val){
+				if(current.left){
+					current = current.left;
+				}
+				else {
+					current.left = newNode;
+					return true;
+				}
+			}
+			else {
+		
+				if(current.right){
+					current = current.right;
+				}else {
+					current.right = newNode;
+					return true;
+				}
+			}
+		}
+	}
+}
